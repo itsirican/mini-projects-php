fe4f87b12901cc334a63e5b9b5d9882602e65182
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-04-23 10:01:35 +0000

    get and implement image page

--- gallery-project/gallery.php
+++ gallery-project/gallery.php
@@ -5,10 +5,10 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
-<?php foreach($imageTitles as $url => $title):?>
-  <a href="image.php?<?=http_build_query(["url" => $url])?>">
-    <h1><?=$title?></h1>
-    <img src="./images/<?php echo rawurlencode($url) ?>" alt="<?=$title?>">
+<?php foreach($imageTitles as $src => $title):?>
+  <a href="image.php?<?=http_build_query(["image" => $src])?>">
+    <h3><?=e($title);?></h3>
+    <img src="./images/<?php echo rawurlencode($src) ?>" alt="<?=e($title);?>">
   </a>
 <?php endforeach; ?>
 
--- gallery-project/image.php
+++ gallery-project/image.php
@@ -6,4 +6,17 @@ include './inc/images.inc.php';
 <?php include './views/header.php'; ?>
 
 
+<?php if(!empty($_GET["image"] && !empty($imageTitles[$_GET["image"]])) ): ?>
+  <?php $image = $_GET["image"] ?>
+  <h2><?php echo e($imageTitles[$image]) ?></h2>
+  <img src="./images/<?php echo rawurlencode($image) ?>">
+  <p><?php echo e($imageDescriptions[$image]) ?></p>
+<?php else: ?>
+  <div class="notice">
+    This image not found.
+  </div>
+<?php endif; ?>
+
+<a href="gallery.php">Back to gallery</a>
+
 <?php include './views/footer.php'; ?>
