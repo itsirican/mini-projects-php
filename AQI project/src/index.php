c78d388f7510596d7a223b9cab884a1970024676
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-04-30 22:43:01 +0000

    transforming json data into web content with dynamic city links

--- AQI project/src/index.php
+++ AQI project/src/index.php
@@ -1,5 +1,16 @@
-<?php require_once __DIR__ . "/inc/functions.inc.php" ?>
+<?php 
+  require_once __DIR__ . "/inc/functions.inc.php" ;
+  $cities = json_decode(file_get_contents(__DIR__."/../data/index.json"), true);
+?>
 
 <?php require_once __DIR__."/views/header.inc.php" ?>
+
+<ul>
+  <?php foreach($cities as $city):?>
+    <li><a href="./city.php?<?=http_build_query(["city" => $city["city"]])?>">
+      <?=$city["city"]?>, <?=$city["country"]?> (<?=$city["flag"]?>)</a>
+    </li>
+  <?php endforeach; ?>
+</ul>
 
 <?php require_once __DIR__."/views/footer.inc.php" ?>
