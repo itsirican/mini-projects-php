e0253b098cfd9db6f4c7aeae6fbaa8a7fd7a5921
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-05-01 23:00:16 +0000

    set table of data

--- AQI project/src/city.php
+++ AQI project/src/city.php
@@ -19,8 +19,21 @@
   if (!empty($filename)) {
     // var_dump($filename);
     $results = json_decode(file_get_contents("compress.bzip2://".__DIR__."/../data/".$filename), true)["results"];
-      var_dump($results[0]);
-    
+      // var_dump($results[0]);
+      $stats = [];
+      foreach($results as $result) {
+        if($result["parameter"] !== "pm25") continue;
+        // var_dump($result);
+        $month = substr($result["date"]["local"], 0, 7);
+        if (!isset($stats[$month])) {
+          $stats[$month] = [];
+        }
+        $stats[$month][] = $result["value"];
+        // var_dump($stats);
+        // var_dump($month);
+        // break;
+      }
+      // var_dump($stats);
   }
 ?>
 
@@ -29,7 +42,18 @@
 <?php if(empty($city)): ?>
   <p>The city could not be loaded.</p>
   <?php else: ?>
-    
+    <?php if (!empty($stats)): ?>
+      <table>
+        <?php foreach($stats as $month => $mesuremenets): ?>
+          <table>
+            <tr>
+              <th><?php echo $month ?></th>
+              <td><?php echo array_sum($mesuremenets) / count($mesuremenets) ?></td>
+            </tr>
+          </table>
+        <?php endforeach; ?>
+      </table>
+    <?php endif; ?>
 <?php endif; ?>
 
 <?php require_once __DIR__."/views/footer.inc.php" ?>
