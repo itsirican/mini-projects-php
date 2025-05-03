80a673c3f6f01ac2c7d44621afc9b8ba6b7edc6f
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-05-03 14:31:50 +0000

    render text data on the website

--- Files project/index.php
+++ Files project/index.php
@@ -17,6 +17,8 @@
             "png"
         ];
         while (($currentDir = readdir($handle)) !== false) {
+            $title = "";
+            $content = "";
             if ($currentDir === "." || $currentDir == ".." || $currentDir === ".DS_Store") continue;
             $extension = pathinfo($currentDir, PATHINFO_EXTENSION);
             // if ($extension !== "jpg" && $extension !== "jpge" && $extension !== "png")
@@ -27,12 +29,18 @@
             $txtFile = __DIR__.'/images/'.$fileWithoutExt.'.txt';
             if (file_exists($txtFile)) {
                 $txt = file_get_contents($txtFile);
+                $infos = explode("\n", $txt);
+                $title = $infos[0];
+                unset($infos[0]);
+                $content = array_values($infos);
+                // var_dump($content);
                 // var_dump($txt);
             }
 
             $images[] = [
                 "image" => $currentDir,
-                "content" => $txt
+                "title" => $title,
+                "content" => $content
             ];
             var_dump($currentDir);
             var_dump($extension);
@@ -41,8 +49,15 @@
         closedir($handle);
     ?></pre>
         <?php foreach($images as $image): ?>
-            <img src="images/<?php echo urlencode($image["image"])?>" alt="">
-            <p><?php echo $image["content"] ?></p>
+            <figure>
+                <h1><?php echo $image["title"] ?></h1>
+                <img src="images/<?php echo urlencode($image["image"])?>" alt="">
+                <figcaption>
+                    <?php foreach($image['content'] as $paragraphs): ?>
+                        <p><?php echo $paragraphs ?></p>
+                    <?php endforeach; ?>
+                </figcaption>
+            </figure>
         <?php endforeach; ?>
     </main>
 </body>
