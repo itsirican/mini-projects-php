4a93692f2c6a85345b5c8071983f0874564ffecf
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-05-05 20:58:33 +0000

    update/delete mysql db

--- Database/notes.php
+++ Database/notes.php
@@ -10,4 +10,13 @@
   $stmt->execute();
   echo "$title added successfuly!"; */
 
-  
+  $title = "note title 1 - updated";
+  $stmt = $pdo->prepare('UPDATE `notes` SET `title` = :title WHERE `id` = :id');
+  $stmt->bindValue("id", 1);
+  $stmt->bindValue("title", $title);
+  $stmt->execute();
+
+  $stmt2 = $pdo->prepare('DELETE FROM `notes` WHERE `id` = :id');
+  $stmt2->bindValue("id", 4);
+  $stmt2->execute();
+  echo "Note 2 deleted successfully";
