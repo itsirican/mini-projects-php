bb339b2985fe06cabb9116346a0a9281b6981263
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-05-06 19:57:58 +0000

    submit data into db

---
+++ Diary prj/views/header.view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./styles/normalize.css" />
+    <link rel="stylesheet" type="text/css" href="./styles/styles.css" />
+    <title>PHP Diary</title>
+</head>
+<body>
+    <nav class="nav">
+        <div class="container">
