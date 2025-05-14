efde610ae5172553af0d12197a9ab3b0b88fbe4a
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-05-14 18:20:45 +0000

    list the names

--- NameExplorer prj/index.php
+++ NameExplorer prj/index.php
@@ -2,9 +2,31 @@
 
 require __DIR__ . '/inc/all.inc.php';
 
+$char = (string) ($_GET['char'] ?? '');
+if (strlen($char) > 1) {
+  $char = $char[0];
+}
+$char = strtoupper($char);
+// var_dump($char);
+$stmt = $pdo->prepare('SELECT DISTINCT `name` FROM `names` WHERE `name` LIKE :exp ORDER BY `name` ASC');
+$stmt->bindValue(":exp", "{$char}%");
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$names = [];
+foreach($results as $result) {
+  $names[] = $result['name'];
+}
+// var_dump($names);
 ?>
 
 <?php require __DIR__ . '/views/header.php'; ?>
-
-
+<ul>
+  <?php foreach($names AS $name): ?>
+    <li>
+      <a href="name.php?<?php echo http_build_query(["name" => $name]) ?>">
+        <?php echo e($name) ?>
+      </a>
+    </li>
+  <?php endforeach; ?>
+</ul>
 <?php require __DIR__ . '/views/footer.php'; ?>
