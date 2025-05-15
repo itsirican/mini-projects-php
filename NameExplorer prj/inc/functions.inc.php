c47fcf6f6bbbb6dc6333d4401cf313bf46171e6d
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-05-15 22:49:05 +0000

    use render function

--- NameExplorer prj/inc/functions.inc.php
+++ NameExplorer prj/inc/functions.inc.php
@@ -4,6 +4,16 @@ function e($value) {
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 
+function render($view, $params) {
+    extract($params);
+
+    ob_start();
+    require __DIR__."/../views/pages/".$view.".php";
+    $content = ob_get_clean();
+
+    require __DIR__."/../views/layouts/main.view.php";
+}
+
 function gen_alphabet() {
     $A = ord('A');
     $Z = ord('Z');
--- NameExplorer prj/name.php
+++ NameExplorer prj/name.php
@@ -6,28 +6,8 @@
     die();
   }
   $entries = fetch_name_entries($name);
-?>
-<?php require __DIR__."/views/header.php"; ?>
-  <?php if(empty($entries)): ?>
-    <p>We could not find any entries in our system :/</p>
-    <?php else: ?>
-      <table>
-        <h1>Statistics for the name: <?php echo e($name); ?></h1>
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Number of babies born</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach($entries AS $entry): ?>
-            <tr>
-              <td><?php echo e($entry['year']); ?></td>
-              <td><?php echo e($entry['count']); ?></td>
-            </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
-  <?php endif; ?>    
-<?php require __DIR__."/views/footer.php"; ?>
 
+  render('name.view', [
+    'name' => $name,
+    'entries' => $entries,
+  ]);
